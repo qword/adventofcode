c0ab7310a2994a7bf51a6768f22a4b2f0fad5ca2
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2018-12-01 22:26:51 +0100

    Solved day 1

---
+++ src/scala/Day1.scala
@@ -0,0 +1,28 @@
+import scala.io.Source
+
+object Day1A {
+  def main(args: Array[String]): Unit = {
+    var tot = 0
+    Source.fromFile("input/day1").getLines.foreach(line => tot += (line toInt))
+    println(tot)
+  }
+}
+
+object Day1B {
+  def main(args: Array[String]): Unit = {
+    var tot = 0
+    var set = scala.collection.mutable.Set[Int]()
+    set += 0
+
+    while (true) {
+      Source.fromFile("input/day1").getLines.foreach(line => {
+        tot += (line toInt)
+        if (set contains tot) {
+          println(s"found: $tot")
+          System.exit(0)
+        }
+        set += tot
+      })
+    }
+  }
+}
