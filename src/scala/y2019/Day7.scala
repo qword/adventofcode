40574af1263967d26c503d1700e86b9081656931
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2019-12-20 12:58:38 +0100

    Refactor day 7a, complete immutable approach

--- src/scala/y2019/Day7.scala
+++ src/scala/y2019/Day7.scala
@@ -1,72 +1,56 @@
 package y2019
 
 import scala.annotation.tailrec
-import scala.io.Source
 
-object Day7s {
+object Day7s extends Input {
   def main(args: Array[String]): Unit = {
     println(s"Part A: $partA") // 65464
   }
 
   def partA: String = {
+    val program = readSingleLine("input/2019/day7")
     (0 to 4).permutations
-      .map(seq => seq.foldLeft(0)((acc, i) => run(readInput, Array(i, acc))))
+      .map(seq => seq.foldLeft(0L)((acc, i) => exec(program, input = List(i, acc)).head))
       .max
       .toString
   }
 
-  def readInput = {
-    val bufferedSource = Source.fromFile("input/2019/day7")
-    try {
-      bufferedSource.getLines.next.split(",").map(_.toInt)
-    } finally {
-      bufferedSource.close()
-    }
-  }
+  @tailrec
+  def exec(
+            program: Map[Long, Long],
+            pointer: Long = 0,
+            input: List[Long] = List.empty,
+            output: List[Long] = List.empty
+          ): List[Long] = {
 
-  def run(program: Array[Int], input: Array[Int]): Int = {
-    var output = 0
-    val inputItr = input.iterator
-
-    @tailrec
-    def exec(pointer: Int): Unit = {
-      val instruction = program(pointer) % 100
-      if (instruction == 99) return
-
-      def getMode(pos: Int): Int = {
-        val inStr = program(pointer) + ""
-        val posInInstr = inStr.length - (2 + pos)
-        if (posInInstr > -1) inStr.charAt(posInInstr).asDigit
-        else 0 // default by specs
-      }
-
-      def getParamValue(pos: Int): Int = {
-        getMode(pos) match {
-          case 0 => program(program(pointer + pos))
-          case 1 => program(pointer + pos)
-        }
-      }
-
-      instruction match {
-        case 1 => program(program(pointer + 3)) = getParamValue(1) + getParamValue(2)
-        case 2 => program(program(pointer + 3)) = getParamValue(1) * getParamValue(2)
-        case 3 => program(program(pointer + 1)) = inputItr.next
-        case 4 => output = getParamValue(1)
-        case 7 => program(program(pointer + 3)) = if (getParamValue(1) < getParamValue(2)) 1 else 0
-        case 8 => program(program(pointer + 3)) = if (getParamValue(1) == getParamValue(2)) 1 else 0
-        case _ =>
-      }
-
-      val nextPointer = instruction match {
-        case 1 | 2 | 7 | 8 => pointer + 4
-        case 3 | 4 => pointer + 2
-        case 5 => if (getParamValue(1) != 0) getParamValue(2) else pointer + 3
-        case 6 => if (getParamValue(1) == 0) getParamValue(2) else pointer + 3
-      }
-      exec(nextPointer)
+    def getMode(pos: Int): Int = {
+      val inStr = program(pointer) + ""
+      val posInInstr = inStr.length - (2 + pos)
+      if (posInInstr > -1) inStr.charAt(posInInstr).asDigit
+      else 0 // default by specs
     }
 
-    exec(0)
-    output
+    def getIndex(pos: Int): Long =
+      getMode(pos) match {
+        case 0 => program(pointer + pos)
+        case 1 => pointer + pos
+      }
+
+    def getVal(pos: Int): Long = program.getOrElse(getIndex(pos), 0)
+
+    val instruction = program(pointer) % 100
+
+    instruction match {
+      case 1 => exec(program + (getIndex(3) -> (getVal(1) + getVal(2))), pointer + 4, input, output)
+      case 2 => exec(program + (getIndex(3) -> (getVal(1) * getVal(2))), pointer + 4, input, output)
+      case 3 => exec(program + (getIndex(1) -> input.head), pointer + 2, input.tail, output)
+      case 4 => exec(program, pointer + 2, input, output :+ getVal(1))
+      case 5 => exec(program, if (getVal(1) != 0L) getVal(2) else pointer + 3, input, output)
+      case 6 => exec(program, if (getVal(1) == 0L) getVal(2) else pointer + 3, input, output)
+      case 7 => exec(program + (getIndex(3) -> (if (getVal(1) < getVal(2)) 1 else 0)), pointer + 4, input, output)
+      case 8 => exec(program + (getIndex(3) -> (if (getVal(1) == getVal(2)) 1 else 0)), pointer + 4, input, output)
+      case 99 => output
+      case _ => List.empty // actually an error but *should never happen* :)
+    }
   }
 }
